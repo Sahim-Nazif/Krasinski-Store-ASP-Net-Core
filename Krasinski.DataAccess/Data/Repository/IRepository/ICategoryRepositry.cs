c95ac663e2d529e6c1e1770817d03efd7b6dde70
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-06-24 13:54:01 -0400

    IRepo implemented

--- Krasinski.DataAccess/Data/Repository/IRepository/ICategoryRepositry.cs
+++ Krasinski.DataAccess/Data/Repository/IRepository/ICategoryRepositry.cs
@@ -6,26 +6,7 @@ using System.Text;
 
 namespace Krasinski.DataAccess.Data.Repository.IRepository
 {
-    public interface ICategoryRepositry<T> where T: class
+    public interface ICategoryRepositry
     {
-        T Get(int id);
-
-        IEnumerable<T> GetAll(
-
-                Expression<Func<T, bool>> filter=null,
-                Func<IQueryable <T>, IOrderedQueryable<T>> orderBy=null,
-                string includeProperties=null
-
-            );
-
-        T GetFirstOrDefault(
-             Expression<Func<T, bool>> filter = null,
-              string includeProperties = null
-            );
-
-        public void Add(T entity);
-        public void Remove(int id);
-        public void Remove(T entity);
-
     }
 }
