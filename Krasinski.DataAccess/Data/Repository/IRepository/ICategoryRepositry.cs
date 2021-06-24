4f2c049055935efcd48c3ca24e14474c8efb0ae1
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-06-24 13:29:22 -0400

    get category & categories

--- Krasinski.DataAccess/Data/Repository/IRepository/ICategoryRepositry.cs
+++ Krasinski.DataAccess/Data/Repository/IRepository/ICategoryRepositry.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Krasinski.DataAccess.Data.Repository.IRepository
 {
-    interface ICategoryRepositry
+    public interface ICategoryRepositry<T> where T: class
     {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+
+                Expression<Func<T, bool>> filter=null,
+                Func<IQueryable <T>, IOrderedQueryable<T>> orderBy=null,
+                string includeProperties=null
+
+            );
+
+        T GetFirstOrDefault(
+             Expression<Func<T, bool>> filter = null,
+              string includeProperties = null
+            );
+
+        public void Add(T entity);
+        public void Remove(int id);
+        public void Remove(T entity);
+
     }
 }
