98c44d995b163ca1f409ab3fdc66aaa0ee99cc66
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-06-25 18:15:30 -0400

    CRUD ops for category

--- Krasinski-Store/Areas/Admin/Controllers/CategoryController.cs
+++ Krasinski-Store/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Krasinski.DataAccess.Data.Repository.IRepository;
+using Krasinski.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -22,7 +23,40 @@ namespace Krasinski_Store.Areas.Admin.Controllers
            
             return View();
         }
+        public IActionResult Upsert(int ? id)
+        {
+            Category category = new Category();
+            if (id == null)
+            {
+                return View(category);
+            }
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
 
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
         #region Calling APIs
         [HttpGet]
         public IActionResult GetAll()
--- Krasinski-Store/wwwroot/js/category.js
+++ Krasinski-Store/wwwroot/js/category.js
@@ -19,13 +19,13 @@ const loadDataTable = () => {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                            <a href="/Admin/category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer;width:100px;">
+                            <a href="/Admin/category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer;width:120px;">
 
-                                <i class='far fa-edit'></i>Edit</a>
+                                <i class='far fa-edit'></i> Edit</a>
                                 &nbsp;
-                            <a onclick=Delete("/Admin/category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer;width:100px;">
+                            <a onclick=Delete("/Admin/category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer;width:130px;">
 
-                                <i class='far fa-trash-alt'></i>Delete</a>
+                                <i class="fas fa-trash"></i> Delete</a>
                             </div>`;
                 }, "width":"30%"
             }
@@ -34,4 +34,36 @@ const loadDataTable = () => {
             "emptyTable":"No records found."
         }, "width": "100%"
     })
+}
+
+const Delete = (url) => {
+    swal({
+        title: "Are you sure you want to proceed ?",
+        text: "You won't be able to restore!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnconfirm:true
+    }, () => {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message)
+                    dataTable.ajax.reload();
+                }
+                else {
+                    toastr.error(data.message)
+                }
+            }
+
+        })
+    })
+}
+
+const showMessage = (msg) => {
+
+   
 }
