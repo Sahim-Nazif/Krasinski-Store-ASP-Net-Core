b50cd98aa60da76c601d2fe5ecb1629038f1928a
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-06-26 12:00:25 -0400

    ajax call to display data

--- Krasinski-Store/Areas/Admin/Controllers/FrequencyController.cs
+++ Krasinski-Store/Areas/Admin/Controllers/FrequencyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Krasinski.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,24 @@ using System.Threading.Tasks;
 
 namespace Krasinski_Store.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class FrequencyController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FrequencyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Frequency.GetAll() });
         }
     }
 }
---
+++ Krasinski-Store/wwwroot/js/frequency.js
@@ -0,0 +1,69 @@
+﻿let dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+})
+
+const loadDataTable = () => {
+    dataTable = $('#tblData').DataTable({
+
+        "ajax": {
+            "url": "/admin/frequency/GetAll",
+            "type": "GET",
+            "dataType": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "50%" },
+            { "data": "frquencycount", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                            <a href="/Admin/frequency/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer;width:120px;">
+
+                                <i class='far fa-edit'></i> Edit</a>
+                                &nbsp;
+                            <a onclick=Delete("/Admin/frequency/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer;width:130px;">
+
+                                <i class="fas fa-trash"></i> Delete</a>
+                            </div>`;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No records found."
+        }, "width": "100%"
+    })
+}
+
+const Delete = (url) => {
+    swal({
+        title: "Are you sure you want to proceed ?",
+        text: "You won't be able to restore!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnconfirm: true
+    }, () => {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message)
+                    dataTable.ajax.reload();
+                }
+                else {
+                    toastr.error(data.message)
+                }
+            }
+
+        })
+    })
+}
+
+const showMessage = (msg) => {
+
+
+}
